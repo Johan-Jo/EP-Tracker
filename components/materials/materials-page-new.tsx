b6f47637dbb7ac213d908c479e9af0d4c4d43bab
Johan-Jo <hej@johan.com.br>
2025-11-13 10:30:58 -0300

    fix: allow undefined ata relation

--- components/materials/materials-page-new.tsx
+++ components/materials/materials-page-new.tsx
@@ -253,7 +253,7 @@ export function MaterialsPageNew({ orgId, projectId, ataDraftId, ataTitle, retur
 		};
 	}, [ataDraftId]);
 	
-	const normalizeSingleRelation = <T,>(relation: T | T[] | null): T | null => {
+	const normalizeSingleRelation = <T,>(relation: T | T[] | null | undefined): T | null => {
 		if (!relation) return null;
 		return Array.isArray(relation) ? relation[0] ?? null : relation;
 	};
